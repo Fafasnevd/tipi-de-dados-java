03b6f5c0120773df8cae9044ae92bd3353f1e5d8
Fafasnevd <rafael.sartori.etec@gmail.com>
2023-02-27 14:29:08 +0000

    salvar

---
+++ Aula-Teste/EstruturadeSelecao.java
@@ -0,0 +1,14 @@
+
+public class EstruturadeSelecao{
+    public static void main(String[] args){
+
+        int idade;
+        idade=19;
+        if ( idade >= 18)
+        {
+            System.out.print("apto ao Serviço Militar Obrigatório");
+        } else {
+            System.out.print("não esta apto ao Servidor militar obrigatório");
+        }
+    }
+}
